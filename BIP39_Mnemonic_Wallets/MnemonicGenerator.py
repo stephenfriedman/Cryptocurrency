3e1706609ebb3def6ea3ce225cc937a715221d3b
stephenfriedman <friedmansemail@gmail.com>
2019-06-15 06:00:54 -0400

    Added commenting

--- BIP39_Mnemonic_Wallets/MnemonicGenerator.py
+++ BIP39_Mnemonic_Wallets/MnemonicGenerator.py
@@ -16,29 +16,38 @@ if inputLength != 256:
 
 print("\nNumber of bits you provided: \n",inputLength)
 bits="0b"+userInput
-a=int(bits, 2)
+# convert the input string to the binary representation
+binary=int(bits, 2)
 
-entropy_bytes = int.to_bytes(a, length=32, byteorder="big")
+# Convert to bytes
+entropy_bytes = int.to_bytes(binary, length=32, byteorder="big")
+# Use sha256 on the bytes
 sha256_bytes = bytes(hashlib.sha256(entropy_bytes).digest())
+# Return the hexadecimal representation of the binary data
 hexResult = binascii.hexlify(sha256_bytes)
 print("\n256 bits hashed with sha256, now represented in hexadecimal:\n",hexResult,"\n")
+# Take the two significant nibbles
 firstTwo = hexResult.decode("utf-8")[:2]
 print("2 most significant digits aka \"2 most significant nibbles\": ",firstTwo,"\n")
+# The checksum is the conversion of these 2 nibbles into binary
 checksum =bin(int(firstTwo, 16))[2:].zfill(8)
 print("----------------------")
 print("CHECKSUM --->",checksum)
 print("----------------------\n")
 
 bits=userInput
+#  Append our 8 bit checksum onto our 256 bits of entropy for a total of 264 bits
 bits += checksum
 
 ElevenBits = ""
 WordNumbers=[]
 WordCounter=0
 mnemonics = []
+# Each 11 bits, convert the binary to ascii and find corresponding word from the list
 for num in range(1,265):
   ElevenBits += bits[num-1]
   decimal = int(ElevenBits, 2)
+  # Split up the 256 bits into 24 11 bit pieces
   if num % 11 == 0:
     WordNumbers.append(decimal)
     WordCounter=WordCounter+1
@@ -46,6 +55,7 @@ for num in range(1,265):
     print("word #",WordCounter," ---> ",ElevenBits," -----> ",decimal," ---> ",wordList[decimal],"\n");
     ElevenBits = ""
 
+# Print out the 24 words
 print("     YOUR 24 WORDS")
 print("----------------------")
 for word in mnemonics:
